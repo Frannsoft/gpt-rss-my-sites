412f6077940a15f36c0c6536f58e89df161b1263
Jordan <jordan@redbrickroadstudio.com>
2023-06-13 22:48:38 -0400

    working

--- netlify/functions/fetch-rss.js
+++ netlify/functions/fetch-rss.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 
 exports.handler = async function (event, context) {
     const response = await fetch('https://www.nintendolife.com/feeds/latest');
-    const data = await response.json();
+    const data = await response.text();
     console.log(data);
 
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: data
     };
 };
