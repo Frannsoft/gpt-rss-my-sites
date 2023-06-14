0a24c735101f16f6dc9f7e9545f4e5c4676d376d
Jordan <jordan@redbrickroadstudio.com>
2023-06-13 22:43:49 -0400

    working

--- netlify/functions/fetch-rss.js
+++ netlify/functions/fetch-rss.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 exports.handler = async function (event, context) {
     const response = await fetch('https://www.nintendolife.com/feeds/latest');
     console.log(response);
